297607b3cb9358dd581c72d1c012f4c29ec5740e
Alexander Prestmo <aprestmo@gmail.com>
2023-02-24 14:56:29 +0100

    Tweaks to boilerplate

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import sanity from "astro-sanity";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity({
-    projectId: 'ae5h6au0',
-    dataset: 'development',
-    // apiVersion: "v2021-03-25",
-    useCdn: true,
-  })]
+  integrations: [
+    sanity({
+      projectId: "ae5h6au0",
+      dataset: "production",
+      apiVersion: "v2021-03-25",
+      useCdn: true,
+    }),
+  ],
 });
--- astro/src/lib/api.js
+++ astro/src/lib/api.js
@@ -1,7 +1,7 @@
 import { useSanityClient, groq } from "astro-sanity";
 
 export async function getFirstBlogPost() {
-  const query = groq`*[_type == "articles"]`;
+  const query = groq`*[_type == "article"] | order(publishedAt desc)`;
   const firstPost = await useSanityClient().fetch(query);
   return firstPost;
 }
@@ -10,4 +10,10 @@ export async function getSiteSettings() {
   const query = groq`*[_type == "siteSettings" && _id == "siteSettings"][0]`;
   const siteSettings = await useSanityClient().fetch(query);
   return siteSettings;
+}
+
+export async function getArticles() {
+  const query = groq`*[_type == "article"] | order(publishedAt desc)`;
+  const articles = await useSanityClient().fetch(query);
+  return articles;
 }
